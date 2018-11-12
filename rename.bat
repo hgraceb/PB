a7bc35728ae32a88727fe16d430851a9c5ec9c91
hgraceb <38378650+hgraceb@users.noreply.github.com>
2018-11-12 16:21:23 +0800

    Update rename.bat

--- rename.bat
+++ rename.bat
@@ -1,6 +1,6 @@
 @echo off & setlocal enabledelayedexpansion
 
-rem ¼ÇÂ¼¿ªÊ¼ÔËĞĞÊ±¼ä
+rem è®°å½•å¼€å§‹è¿è¡Œæ—¶é—´
 set begin=%time%
 if %time:~0,2% lss 10 (
     set /a hh=10%time:~1,1%-100
@@ -15,7 +15,7 @@ if %time:~0,2% equ 0 set /a time1+=8640000
 
 rem cl rename.c
 
-echo ÕıÔÚÖØÃüÃûÎÄ¼ş. . .
+echo æ­£åœ¨é‡å‘½åæ–‡ä»¶. . .
 set /p=________________________________________________________________________________<nul
 echo.
 set count=0
@@ -25,10 +25,10 @@ for /f "tokens=*" %%a in ('dir /b *.avf') do (
 )
 
 
-rem ¼ÆËãÔËĞĞÓÃÊ±
+rem è®¡ç®—è¿è¡Œç”¨æ—¶
 rem echo.
-rem echo ¿ªÊ¼Ê±¼ä£º%begin%
-rem echo ½áÊøÊ±¼ä£º%time%
+rem echo å¼€å§‹æ—¶é—´ï¼š%begin%
+rem echo ç»“æŸæ—¶é—´ï¼š%time%
 if %time:~0,2% lss 10 (
     set /a hh=10%time:~1,1%-100
 )else (
@@ -43,32 +43,28 @@ if %time:~0,2% equ 0 set /a time2+=8640000
 set/a time3=%time2%-%time1%
 set/a speed=!count!*10000/%time3%
 
-rem ¸öÎ»ÓÃ0²¹×ã
+rem ä¸ªä½ç”¨0è¡¥è¶³
 set/a second=%time3:~,-2%+0
 set/a unit=%speed:~,-2%+0
 
 set /p=________________________________________________________________________________<nul
 echo.
-echo ¹²ÖØÃüÃûÎÄ¼ş !count! ¸ö, ËÙ¶È !unit!.%speed:~-2,2% ¸ö/s, ×ÜºÄÊ±: !second!.%time3:~-2,2% s
+echo å…±é‡å‘½åæ–‡ä»¶ !count! ä¸ª, é€Ÿåº¦ !unit!.%speed:~-2,2% ä¸ª/s, æ€»è€—æ—¶: !second!.%time3:~-2,2% s
 echo.
 
 if exist pb.bat (
-echo 10sºó×Ô¶¯¿ªÊ¼²éÕÒPB. . .
-ping /n 4 127.0.0.1> nul
-echo 8s ºó×Ô¶¯¿ªÊ¼²éÕÒPB. . .
-ping /n 4 127.0.0.1> nul
-echo 6s ºó×Ô¶¯¿ªÊ¼²éÕÒPB. . .
-ping /n 4 127.0.0.1> nul
-echo 4s ºó×Ô¶¯¿ªÊ¼²éÕÒPB. . .
-ping /n 4 127.0.0.1> nul
-echo 2s ºó×Ô¶¯¿ªÊ¼²éÕÒPB. . .
-ping /n 4 127.0.0.1> nul
+echo 3s åè‡ªåŠ¨å¼€å§‹æŸ¥æ‰¾PB. . .
+ping /n 2 127.0.0.1> nul
+echo 2s åè‡ªåŠ¨å¼€å§‹æŸ¥æ‰¾PB. . .
+ping /n 2 127.0.0.1> nul
+echo 1s åè‡ªåŠ¨å¼€å§‹æŸ¥æ‰¾PB. . .
+ping /n 2 127.0.0.1> nul
 
-rem set /p=Çë°´ÈÎÒâ¼ü¿ªÊ¼²éÕÒPB. . .<nul
+rem set /p=è¯·æŒ‰ä»»æ„é”®å¼€å§‹æŸ¥æ‰¾PB. . .<nul
 rem pause > nul
 
 start pb.bat
 )else (
-set /p=Çë°´ÈÎÒâ¼üÍË³ö. . .<nul
+set /p=è¯·æŒ‰ä»»æ„é”®é€€å‡º. . .<nul
 pause >nul
 )
